6ef6e2d94c170f446f13a6370f6f7649aa63ee58
Siddharath  Bhardwaj <50177404+sobhardwaj@users.noreply.github.com>
2023-02-28 21:34:25 +0000

    clean it up..

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import { Button } from '@mantine/core'
 
 function App() {
   return (
-    // <MantineProvider withGlobalStyles withNormalizeCSS>
-    //    <div className="App">
-    //  <p>demo React app ..</p>
-    //  <Button>Join</Button>
-    // </div>
-    // </MantineProvider>
      <MantineProvider
      withGlobalStyles
      withNormalizeCSS
