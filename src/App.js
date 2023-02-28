8e6ce626273f0a4876a4b903938af4a4e7cfc28f
Siddharath  Bhardwaj <50177404+sobhardwaj@users.noreply.github.com>
2023-02-28 21:13:22 +0000

    added mantine library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { MantineProvider } from '@mantine/core';
+import { Button } from '@mantine/core'
 
 function App() {
   return (
-    <div className="App">
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+       <div className="App">
      <p>demo React app ..</p>
+     <Button>Join</Button>
     </div>
+    </MantineProvider>
+   
   );
 }
 
